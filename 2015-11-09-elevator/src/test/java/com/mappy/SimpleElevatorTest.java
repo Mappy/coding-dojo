96f252856cb0141919a3e805f5b09a25240ecebd
Guillaume Rose <guillaume.rose@mappy.com>
2015-11-18 16:30:01 +0100

    more clean up

--- 2015-11-09-elevator/src/test/java/com/mappy/SimpleElevatorTest.java
+++ 2015-11-09-elevator/src/test/java/com/mappy/SimpleElevatorTest.java
@@ -1,42 +1,28 @@
 package com.mappy;
 
+import java.util.List;
+
 import org.junit.Test;
 
+import static com.google.common.collect.Lists.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class SimpleElevatorTest {
     private final Elevator elevator = new SimpleElevator();
 
     @Test
-    public void should_be_true() throws Exception {
-        assertThat(elevator.nextCommand()).isEqualTo("OPEN");
-        assertThat(elevator.nextCommand()).isEqualTo("CLOSE");
-        assertThat(elevator.nextCommand()).isEqualTo("UP");
+    public void should_do_omnibus() {
+        for (int i = 0; i < 5; i++) {
+            assertCommands(elevator, newArrayList("OPEN", "CLOSE", "UP"));
+        }
+        assertCommands(elevator, newArrayList("OPEN", "CLOSE", "DOWN", "DOWN", "DOWN", "DOWN", "DOWN", "OPEN"));
     }
 
-    @Test
-    public void should_be_true_with_stage() throws Exception {
-        for (int i = 0; i < 5; i++) {
-            assertThat(elevator.nextCommand()).isEqualTo("OPEN");
-            assertThat(elevator.nextCommand()).isEqualTo("CLOSE");
-            assertThat(elevator.nextCommand()).isEqualTo("UP");
+    public void assertCommands(Elevator elevator, List<String> commands) {
+        List<String> answers = newArrayList();
+        for (int i = 0; i < commands.size(); i++) {
+            answers.add(elevator.nextCommand());
         }
-    }
-
-    @Test
-    public void should_return_to_0_after_stage_5() {
-        for (int i = 0; i < 5; i++) {
-            assertThat(elevator.nextCommand()).isEqualTo("OPEN");
-            assertThat(elevator.nextCommand()).isEqualTo("CLOSE");
-            assertThat(elevator.nextCommand()).isEqualTo("UP");
-        }
-        assertThat(elevator.nextCommand()).isEqualTo("OPEN");
-        assertThat(elevator.nextCommand()).isEqualTo("CLOSE");
-        assertThat(elevator.nextCommand()).isEqualTo("DOWN");
-        assertThat(elevator.nextCommand()).isEqualTo("DOWN");
-        assertThat(elevator.nextCommand()).isEqualTo("DOWN");
-        assertThat(elevator.nextCommand()).isEqualTo("DOWN");
-        assertThat(elevator.nextCommand()).isEqualTo("DOWN");
-        assertThat(elevator.nextCommand()).isEqualTo("OPEN");
+        assertThat(answers).containsExactlyElementsOf(commands);
     }
 }
